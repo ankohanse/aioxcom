1ea2dd87e6177448ef01d7654c98dc69f940d178
Anko Hanse <anko_hanse@hotmail.com>
2025-01-06 14:00:52 +1300

    Improve example

--- example_api_use.py
+++ example_api_use.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 async def main():
     dataset = await XcomDataset.create(VOLTAGE.AC240) # or use VOLTAGE.AC120
     info_3023 = dataset.getByNr(3023, "xt")  # the "xt" part is optional but usefull for detecting mistakes
-    info_6001 = dataset.getByNr(6001, "bsp")
+    info_7002 = dataset.getByNr(7002, "bsp")
     param_5012 = dataset.getByNr(5012, "rcc")
     param_1107 = dataset.getByNr(1107, "xt")
     dataset = None  # Release memory of the dataset
@@ -25,23 +25,23 @@ async def main():
 
         # Retrieve info #3023 from the first Xtender (Output power)
         value = await api.requestValue(info_3023, "XT1")    # xt address range is 101 to 109, or use "XT1" to "XT9"
-        logger.info(f"XT1 3023: {value} {info_3023.unit} ({info_3023.name})")
+        logger.info(f"XT1 {info_3023.nr}: {value} {info_3023.unit} ({info_3023.name})")
 
-        # Retrieve param #6001 from BSP (Nominal capacity)
-        value = await api.requestValue(info_6001, "BSP")    # bsp address range is only 601, or use "BSP"
-        logger.info(f"BSP 6001: {value} {info_6001.unit} ({info_6001.name})")
+        # Retrieve info #7002 from BSP (State of Charge)
+        value = await api.requestValue(info_7002, "BSP")    # bsp address range is only 601, or use "BSP"
+        logger.info(f"BSP {info_7002.nr}: {value} {info_7002.unit} ({info_7002.name})")
 
         # Retrieve param #5012 from RCC (User Level)
         value = await api.requestValue(param_5012, "RCC")    # rcc address range is only 501, or use "RCC"
-        logger.info(f"RCC 5012: {param_5012.enum_value(value)} ({param_5012.name})")
+        logger.info(f"RCC {param_5012.nr}: {param_5012.enum_value(value)} ({param_5012.name})")
 
         # Retrieve and Update param 1107 on the first Xtender (Maximum current of AC source)
         value = await api.requestValue(param_1107, "XT1")
-        logger.info(f"XT1 1107: {value} {param_1107.unit} ({param_1107.name})")
+        logger.info(f"XT1 {param_1107.nr}: {value} {param_1107.unit} ({param_1107.name})")
 
         value = 4.0    # 4 Ampere
         if await api.updateValue(param_1107, value, "XT1"):
-            logger.info(f"XT1 1107 updated to {value} {param_1107.unit} ({param_1107.name})")
+            logger.info(f"XT1 {param_1107.nr} updated to {value} {param_1107.unit} ({param_1107.name})")
 
     except Exception as e:
         logger.info(f"Unexpected exception: {e}")
