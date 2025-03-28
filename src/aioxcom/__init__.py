411c62a70753e6310084fee4333e35e8315e4b0b
Anko Hanse <anko_hanse@hotmail.com>
2025-03-28 16:27:44 +1300

    Add missing export

--- src/aioxcom/__init__.py
+++ src/aioxcom/__init__.py
@@ -1,7 +1,7 @@
 from .xcom_const import VOLTAGE, LEVEL, FORMAT, OBJ_TYPE, SCOM_OBJ_TYPE, SCOM_SERVICE, SCOM_QSP_ID, SCOM_QSP_LEVEL, SCOM_AGGREGATION_TYPE, SCOM_ERROR_CODES
 from .xcom_api import XcomApiTcp, XcomApiWriteException, XcomApiReadException, XcomApiTimeoutException, XcomApiUnpackException, XcomApiResponseIsError
 from .xcom_datapoints import XcomDataset, XcomDatapoint, XcomDatapointUnknownException
-from .xcom_discover import XcomDiscover, XcomDiscoveredDevice
+from .xcom_discover import XcomDiscover, XcomDiscoveredClient, XcomDiscoveredDevice
 from .xcom_families import XcomDeviceFamily, XcomDeviceFamilies, XcomDeviceFamilyUnknownException, XcomDeviceCodeUnknownException, XcomDeviceAddrUnknownException
 from .xcom_protocol import XcomPackage, XcomHeader, XcomFrame, XcomService
 from .xcom_protocol import XcomData, XcomDataMessageRsp, XcomDataMultiInfoReq, XcomDataMultiInfoReqItem, XcomDataMultiInfoRsp, XcomDataMultiInfoRspItem
